febf45f6a6a0b33482f130d25c826a541ac52b13
muhammad fauzi masykur <muhammad.masykur@gojek.com>
2024-09-26 23:32:17 +0700

    expose SnapBi public class for using single entry of the library

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const Snap = require('./lib/snap');
 const CoreApi = require('./lib/coreApi');
 const Iris = require('./lib/iris');
 const MidtransError = require('./lib/midtransError');
+const {SnapBiConfig, SnapBi} = require("./SnapBiClient");
 const Midtrans = { 
 	Snap, 
 	CoreApi, 
 	Iris, 
-	MidtransError 
+	MidtransError,
 }
 
-module.exports = Midtrans;
+module.exports = {
+	Midtrans,
+	SnapBiConfig,
+	SnapBi
+};
