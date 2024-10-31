9863d33729960a2547217e31682660c925e45dd5
muhammad fauzi masykur <muhammad.masykur@gojek.com>
2024-10-31 18:25:49 +0700

    fix library entry point for SnapBi

--- SnapBiClient/index.js
+++
@@ -1,8 +0,0 @@
-// src/index.js
-const SnapBiConfig = require("../lib/SnapBi/snapBiConfig");
-const SnapBi = require("../lib/SnapBi/snapBi");
-
-module.exports = {
-    SnapBiConfig,
-    SnapBi
-};
